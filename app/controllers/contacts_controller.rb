4424795bd050d561fcb06572a1fccf804dc6573d
= <=>
2016-03-03 08:04:35 -0500

    Create contact form

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render 'new'  
+    end
+  end
+
+  def edit
+  end
+  
+  private
+  
+  def contact_params
+    params.require(:contact).permit(:name, :email, :age, :state, :role)
+  end
+end
